b8367b0ad9b43752a69626b265541bfea9ba3d9e
jshields <shields.h.josh@gmail.com>
2016-01-15 22:24:04 -0600

    work on binary tree

--- shields/util/data_structure.py
+++ shields/util/data_structure.py
@@ -8,181 +8,241 @@ Python has most of this stuff built in already.
 
 class InfoPacket(object):
 
-	def __init__(self, value=None):
-		self.value = value
+    def __init__(self, value=None):
+       self.value = value
 
-	@property
-	def value(self):
-		return self._value
+    @property
+    def value(self):
+       return self._value
 
-	@value.setter
-	def value(self, val):
-		self._value = val
+    @value.setter
+    def value(self, value):
+       self._value = value
 
-	@value.deleter
-	def value(self):
-		del self._value
-
-class Collection(object):
-
-	def __init__(self, items=None):
-		
-		if items is None:
-			items = []
-
-		if type(items) != list:
-			logging.warning('Non-list passed into Collection. May not function as intended.')
-
-		self._items = items
-				
-
-	def __str__(self):
-		return "<Collection (%s)>" % (self._items)
-
-	@classmethod
-	def length(self):
-		return len(self._items)
-	
-#	def length(self):
-#		# count how many nodes are in the list
-#		length = 1 # include head in length
-#		tmp = self.head
-#		while tmp.next is not None
-#			length+=1
-#			tmp.next()
-#		# return length of list
-#		return length
+    @value.deleter
+    def value(self):
+       del self._value
 
 
-#	class Node(object):
-#		def __init__(self, link_style=None, value=None, left=None, right=None, parent=None, linked_nodes=None):
-#			if linked_nodes is None:
-#				linked_nodes = []
-#			pass
+class LinkedList(object):
+    """single Linked List data structure"""
+    
+    class Node:
+        """individual node within a Linked List"""
+       def __init__(value, next_node=None):
+            """initialize a node"""
+         # value could be anything, including an object
+         self.value = value
+         self.next_node = next_node
 
+    def __init__(self, items=None, head):
+        """initialize LinkedList"""
+       if items is None:
+         items = []
 
-class LinkedList(Collection):
-	# Linked List - Single
-	class Node:
-		def __init__(value, next_node=None):
-			# value could be anything, including an object
-			self.value = value
-			self.next_node = next_node
+       if type(items) != list:
+         logging.warning('Non-list passed into Collection. May not function as intended.')
 
-	def __init__(self, items=None, head):
-		if items is None:
-			items = []
+       self._items = items
+          
 
-		if type(items) != list:
-			logging.warning('Non-list passed into Collection. May not function as intended.')
+       self.head = head
 
-		self._items = items
-				
-
-		self.head = head
-
-	def next:
-		# go to next node
-		pass
+    def next:
+       # go to next node
+       pass
 
 
 
-# Double should implement 'previous' method
-class DoubleLinkedList(LinkedList):
-	class Node:
-		def __init__(value=None, left=None, right=None):
-			pass
 
-	# DoubleLinkedList methods
-	def __init__(self):
-		pass
+class DoubleLinkedList(object):
+    class Node:
+       def __init__(value=None, left=None, right=None):
+         pass
 
-	def previous(self):
-		pass
+    # DoubleLinkedList methods
+    def __init__(self):
+       pass
+
+    def next:
+        """go to next node"""
+        pass
+
+    def previous(self):
+        """go to previous node"""
+       pass
 
 # pip install bintrees - would provide a preferred implementation
-class BinaryTree(Collection):
-	class Node:
-		def __init__(self, value=None, left=None, right=None, parent=None):
-			pass
 
 
-	def __init__(self, root):
-			# store the root node with its starting value
-			self.root = Node(root)
+class BinaryTree(object):
+    """Binary Tree"""
+
+    class BinaryTreeRootException(BaseException):
+        """Exception related to the root Node"""
+        pass
+
+    class BinaryTreeNodeException(BaseException):
+        """Exception related to a Node"""
+        pass
+
+    class Node:
+        """Node for Binary Tree"""
+        def __init__(self, parent=None, value=None, left=None, right=None):
+            self._parent = parent
+
+            if self._parent == None:
+                self._isrootlike = True
+            else:
+                self._isrootlike = False
+
+            self._value = value
+            self._left = left
+            self._right = right
+
+        @property
+        def parent(self):
+            return self._parent
+
+        @parent.setter
+        def parent(self, value):
+
+            if value == None:
+                self._isrootlike = True
+            else:
+                self._isrootlike = False
+                if not isinstance(value, Node):
+                    raise BinaryTreeRootException('Cannot set root to instance other than of type Node.')
+
+            self._parent = value
+        
+
+        @property
+        def left(self):
+            """get the node to the left"""
+            return self._left
+
+        @left.setter
+        def left(self, value):
+            """set value of the left Node"""
+            if isinstance(value, Node):
+                self._left = value
+            else:
+                raise BinaryTreeNodeException('Attempted insertion of non-Node.')
+
+        @property
+        def right(self):
+            """get the node to the right"""
+            return self._right
+
+        @right.setter
+        def right(self, node_value):
+            """set value of the right Node"""
+            self._right = Node(node_value)
+
+    def __init__(self, root=None):
+        """initialize Binary Tree"""
+        # a Binary Tree must have at minimum a single root Node
+        if (not isinstance(root, Node)) and (root != None):
+            raise BinaryTreeRootException('Invalid root. Must be a Node.')
+        if root == None:
+            self._root == Node()
+
+    def root(self):
+        """get the root Node / head"""
+        return self._root
+        
+
+    #   def add(self, child):
+    #       if child.value < parent.value
+    #       n = Node(value=child)
 
 
+    def remove(self, child_id):
+       pass
 
-	def add(self, child):
-		if child.value < parent.value
-		n = Node(value=child)
+    def find(self, child):
+       pass
 
+    @property
+    def depth(self):
+        #deepest = 0
+        #for
+        #return deepest
 
-	def remove(self, child_id):
-		pass
-
-
-	def find(self, child):
-		pass
-
-	def traverse(self, starting=self.root):
-		# recursively follow branches
-		pass
-
-	@property
-	def max_depth(self):
-		#deepest = 0
-		#for
-	    #return deepest
-	
-
-	def total(self):
-		# return the total number of children plus the top parent
-		pass
+    def print_tree(self):
+        """draw a visual representation of the tree"""
+        #        o
+        #       / \
+        #      o   o
+        #     / \ / \
+        pass
 
 
 # LIFO
 class Stack(Collection):
-	"""Stack data structure implementation"""
+    """Stack data structure implementation"""
 
-	def __init__(self, items, max_size=None):
-		self.max_size = max_size
-		
+    def __init__(self, items, max_size=None):
+       self.max_size = max_size
+       
 
-	# length method of parent class should work
+    # length method of parent class should work
 
-	def push(self, item):
-		# push onto the top of the stack
-		# equivalent to append()
-		pass
+    def push(self, item):
+       # push onto the top of the stack
+       # equivalent to append()
+       pass
 
-	def pop(self):
-		# pop the top one off of the stack
-		# equivalent to pop() method of the python list
-		tmp = self._items[-1]
-		self.internal = self._items[:-1]
-		return tmp
+    def pop(self):
+       # pop the top one off of the stack
+       # equivalent to pop() method of the python list
+       tmp = self._items[-1]
+       self.internal = self._items[:-1]
+       return tmp
 
-	def peek(self):
-		tmp = self._items[-1]
-		return tmp
+    def peek(self):
+       tmp = self._items[-1]
+       return tmp
 
 
 # collections.deque is preferred
 # FIFO
 class Queue(Collection):
-	
-	def __init__(self, max_size=None):
-		pass
+    
+    def __init__(self, max_size=None):
+       pass
 
-	def enqueue(self, item):
-		# put the item at the back of the queue
-		#tmp = [item]
-		# shift over by one to make room
-		#for i in self.internal:
-		#	tmp[i+1] = self.internal[i] # nope: out of range
-		#self.internal = tmp
-		pass
+    def enqueue(self, item):
+       # put the item at the back of the queue
+       #tmp = [item]
+       # shift over by one to make room
+       #for i in self.internal:
+       # tmp[i+1] = self.internal[i] # nope: out of range
+       #self.internal = tmp
+       pass
 
-	def dequeue(self, item):
-		pass
+    def dequeue(self, item):
+       pass
+
+
+
+def main():
+    """for during development only"""
+    print('starting smoke test')
+
+    bintree = BinaryTree(root=Node('root node val'))
+
+    root = bintree.root
+
+    root.left = Node('left node val')
+    root.right = Node('right node val')
+
+    root.left.left = Node('left left node val')
+    root.left.right = Node('left right node val')
+
+
+
+
+if __name__ == '__main__':
+    main()
